b8524ae9565e83c0bdb8b703402e32a26950aba4
avpizarro <avpizarro@live.com>
2021-02-06 17:41:16 +1100

    Add html routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = (app) => {
+
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/assets/index.html'));
+    });
+
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/assets/notes.html'));
+    });
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/assets/index.html'));
+    });
+}
+
