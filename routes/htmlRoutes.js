0763d8acc89ba95de0127deb82c4b6545b7d811d
avpizarro <avpizarro@live.com>
2021-02-07 16:37:58 +1100

    Move index.html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = (app) => {
 
     app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
+        res.sendFile(path.join(__dirname, './index.html'));
     });
 
     app.get('/notes', (req, res) => {
@@ -11,7 +11,7 @@ module.exports = (app) => {
     });
     
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
+        res.sendFile(path.join(__dirname, './index.html'));
     });
 }
 
